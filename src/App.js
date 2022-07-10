9462f839688f89a0460bd0f3c35e3e422e2cf824
daikiace05 <manirajendran425@gmail.com>
2022-07-10 23:34:21 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Manager Contact Page</h1>
+    </div>
+  );
+}
+
+export default App;
